40d3ec00cb18f511a604466c6617530d72f78a4c
guillaumelortie <guillaumelortie@gmail.com>
2011-10-12 14:01:47 -0400

    Finished first cut of the Uer model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round",:height => "100" , :width => "100")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
